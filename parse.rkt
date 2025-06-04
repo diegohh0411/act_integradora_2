df151edac9fc4cf973df79d4501c7f3b46af9e8c
Diego Hernández Herrera <diegohh0411@gmail.com>
2025-06-04 17:47:18 -0600

    Refactor NFA structure and remove unused parse functionality

--- parse.rkt
+++
@@ -1,9 +0,0 @@
-
-(require racket/string)
-(require racket/port)
-
-;; Función para leer el contenido de un archivo
-(define (read-file filepath)
-  (call-with-input-file filepath
-    (lambda (in)
-      (port->string in))))
